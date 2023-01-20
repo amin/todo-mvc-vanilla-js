46424937b29fe940944493ab7a065d6a8f879fbd
Amin El-Rifai <amin@MacBook-Pro-som-tillhor-Amin.local>
2023-01-20 22:22:34 +0100

    more cleanup

--- src/Todo/Todo.app.js
+++ src/Todo/Todo.app.js
@@ -1,27 +1,33 @@
-import { TodoView as View } from './Todo.view.js'
-import { TodoModel as Model } from './Todo.model.js'
-import { todoFactory } from './components/index.js'
+import { TodoView as View } from "./Todo.view.js";
+import { TodoModel as Model } from "./Todo.model.js";
+import { todoFactory } from "./components/index.js";
 
 export class Todo {
     constructor() {
-        this.view = new View;
-        this.model = new Model;
+        this.view = new View();
+        this.model = new Model();
     }
 
     initialise = () => {
         todoFactory.todos = this.model.todos;
         this.view.initialise();
         todoFactory.build();
-    }
+    };
 
     bindEvents = () => {
-        todoFactory.on('input[name="checkbox"]', 'click', ((e) => console.log(e.currentTarget)))
-        todoFactory.on('input[name="task"]', 'click', ((e) => console.log(e.currentTarget)))
-        todoFactory.on('input[name="destroy"]', 'click', ((e) => console.log(e.currentTarget)))
-    }
+        todoFactory.on('input[name="checkbox"]', "click", (e) =>
+            console.log(e.currentTarget)
+        );
+        todoFactory.on('input[name="task"]', "click", (e) =>
+            console.log(e.currentTarget)
+        );
+        todoFactory.on('input[name="destroy"]', "click", (e) =>
+            console.log(e.currentTarget)
+        );
+    };
 
     render = () => {
         this.view.renderElements(todoFactory.elements);
-        this.bindEvents()
-    }
+        this.bindEvents();
+    };
 }
